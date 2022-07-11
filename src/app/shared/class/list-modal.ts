d9c3f24eb3db69e7dd993821588aeb571b425117
Claive Monteza <43734209+claivemonteza@users.noreply.github.com>
2022-07-11 07:32:43 +0200

    Modify the class

--- src/app/shared/class/list-modal.ts
+++ src/app/shared/class/list-modal.ts
@@ -1,32 +1,34 @@
-import { FormGroup, FormControl } from '@angular/forms';
-import { Output, EventEmitter, NgZone, Injectable } from '@angular/core';
+import { Output, EventEmitter, Injectable, Input } from '@angular/core';
 import { IAgendar } from 'src/app/domain/agendar';
 
 @Injectable()
 export class ListModal<T>  {
-    isVisible: boolean = false;
-    loading: boolean = false;
-    source: IAgendar[] = [];
+  isVisible: boolean = false;
+  loading: boolean = false;
+  updateVisible: boolean = false;
+  value: any = '';
+  marcacao!: any;
+  @Output() newEvent = new EventEmitter<T>();
 
-    marcacao: any;
-    
-    @Output() new = new EventEmitter<T>();
-    @Output() elements = new EventEmitter<T[]>();
+  openModal() { this.isVisible = true; }
+  cancelModal() { this.isVisible = false; this.value = ''; }
 
+  list(reset?: boolean) { }
 
-    openModal() { this.isVisible = true; }
-    cancelModal() { this.isVisible = false;}
+  onAddedElement(t: T) {
+    this.marcacao.agendamentos.push(t);
+    console.log(this.marcacao.agendamentos);
+    /*this.newEvent.emit(t);*/
+  }
 
-    list(reset?: boolean) { }
+  selectElement(t: T) {
+    this.updateVisible = true;
+    this.value = '';
+    this.value = t;
+  }
 
-    onAddedElement(t: T) { 
-        this.marcacao.agendar = [...this.marcacao.agendar, t];
-        this.new.emit(t);
-      }
-
-      selectElement(t: T) { }
-
-      onDeletedElement(date:Date, info:string) {
-        this.marcacao.agendar= this.marcacao.agendar.filter((a: IAgendar) => a.date!==date && a.informacao !== info);
-      }
+  onDeletedElement(date: Date, info: string) {
+    this.marcacao.agendamentos = this.marcacao.agendamentos?.filter((a: IAgendar) => a.date !== date && a.informacao !== info);
+    this.value = '';
+  }
 }
